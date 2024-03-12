0ae294e6a43c556e037dc582130aed2ba5762fdd
Abbie Sarmento <abbiesarmento@u.boisestate.edu>
2024-03-12 11:13:17 -0600

    Added files/methods

---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,14 @@
+package fa.nfa;
+
+import fa.State;
+
+public class NFAState extends State {
+
+    public  NFAState (){
+        super("");
+    }
+
+    public NFAState(String name){
+        super(name);
+    }
+}
